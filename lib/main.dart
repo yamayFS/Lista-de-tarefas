e96c12a951d46cffb9f282f2d95628a49c9b91ea
Yamay Freitas <yamayfreitas@gmail.com>
2021-08-26 18:24:26 -0300

    Inserção de funções Future

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,45 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:io';
 
-void main(){
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() {
   runApp(MaterialApp(
     home: Home(),
   ));
 }
 
 class Home extends StatefulWidget {
-
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  List _toDoList = [];
+
   @override
   Widget build(BuildContext context) {
     return Container();
+  }
+
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+
+  Future<File> _saveData() async {
+    String data = json.encode(_toDoList);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  Future<String> _readData() async {
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (e) {
+      return null;
+    }
   }
 }
